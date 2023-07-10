16f4d80d79f70ac8bda526d0f3c57bdc668907b8
sreekar-muktevi <hi@899000.xyz>
2023-07-10 19:29:24 -0400

    hi

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, Text, View, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-
+// It can be any of these: arrival-estimates, agencies, routes, segments, stops, vehicles
+async function hi() {
+  const url =
+    'https://transloc-api-1-2.p.rapidapi.com/vehicles.json?agencies=1323';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '2db6175e9bmsh7aab84ab1852e64p1da0cdjsnd0476b706254',
+      'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com',
+    },
+  };
+  return fetch(url, options)
+    .then(response => response.json())
+    .catch(error => {
+      console.error(error);
+    });
+}
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await hi();
+      setData(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {/* {JSON.stringify(data)} */}
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <React.Fragment>
+          <MapView
+            style={styles.map}
+            //specify our coordinates.
+            initialRegion={{
+              latitude: 40.5008,
+              longitude: -74.4474,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+          <Text style="font-size: 150px">{JSON.stringify(data)}</Text>
+        </React.Fragment>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    width: '100%',
-    height: '100%',
+    ...StyleSheet.absoluteFillObject,
   },
 });
+
