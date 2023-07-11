4c248a6530140a21256fc482ecbee330686ee597
sreekar-muktevi <hi@899000.xyz>
2023-07-10 20:56:27 -0400

    hi

--- datataker.js
+++ datataker.js
@@ -1,28 +1,46 @@
 const http = require('https');
 
-// It can be any of these: arrival-estimates, agencies, routes, segments, stops, vehicles
-const whichdata = "stops";
+function getTranslocData(whichdata) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'GET',
+      hostname: 'transloc-api-1-2.p.rapidapi.com',
+      port: null,
+      path: `/${whichdata}.json?agencies=1323`,
+      headers: {
+        'X-RapidAPI-Key': '2db6175e9bmsh7aab84ab1852e64p1da0cdjsnd0476b706254',
+        'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com'
+      }
+    };
 
-const options = {
-	method: 'GET',
-	hostname: 'transloc-api-1-2.p.rapidapi.com',
-	port: null,
-	path: `/${whichdata}.json?agencies=1323`,
-	headers: {
-		'X-RapidAPI-Key': '2db6175e9bmsh7aab84ab1852e64p1da0cdjsnd0476b706254',
-		'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com'
-	}
-};
+    const req = http.request(options, function (res) {
+      const chunks = [];
 
-const req = http.request(options, function (res) {
-	const chunks = [];
+      res.on('data', function (chunk) {
+        chunks.push(chunk);
+      });
 
-	res.on('data', function (chunk) { chunks.push(chunk) });
+      res.on('end', function () {
+        const body = Buffer.concat(chunks);
+        resolve(body.toString());
+      });
+    });
 
-	res.on('end', function () {
-		console.log(JSON.parse(Buffer.concat(chunks).toString()));
-	});
-});
+    req.on('error', function (err) {
+      reject(err);
+    });
 
+    req.end();
+  });
+}
 
-req.end()
+// Usage
+const whichdata = "arrival-estimates";
+
+getTranslocData(whichdata)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
