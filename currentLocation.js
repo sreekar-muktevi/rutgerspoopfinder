124a738ebeac77e776a96f8f17a84b40d9f3a69c
Andrea Kim <80376288+jinmisul@users.noreply.github.com>
2023-07-10 21:07:35 -0400

    enter address input

--- currentLocation.js
+++ currentLocation.js
@@ -42,7 +42,7 @@ export default function CurrentLocation() {
   return (
     <View style={styles.container}>
       <TextInput
-        placeholder="Address"
+        placeholder="Enter Address"
         value={address}
         onChangeText={setAddress}
       />
